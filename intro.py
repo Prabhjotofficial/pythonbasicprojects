0557199073f185fe042277c484869b99edc26590
Prabhjotofficial <127003360+Prabhjotofficial@users.noreply.github.com>
2023-03-05 10:09:16 +0530

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,13 @@
+#This is the intro file to the projects
+
+print(" # # # # # # # # # # # # # # # ")
+print(" #                           #")
+print(" #\t WELCOME TO MY WORLD!\t #")
+print(" #\t    You will find    \t #")
+print(" #\t  some projects made \t #")
+print(" #\t       by ME!        \t #")
+print(" #\t   -PRABHJOT SINGH   \t #")
+print(" #                           #")
+print(" # # # # # # # # # # # # # # # ")
+
+#Let's start!
